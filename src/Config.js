197db405714620f368c0e75a49fe26593c82d857
Miguel <mlezcano1985@users.noreply.github.com>
2019-07-11 08:41:29 -0400

    updated

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 const Config = {
     baseUrl: (process.env.NODE_ENV === 'production') ? 'https://floating-woodland-62237.herokuapp.com' : `${window.location.protocol}//${window.location.hostname}:8080`,
-    mapKey: 'AIzaSyCnKFbWV13tenNFJ1ANVX5pGCNJ5VkMzM0'
+    mapKey: 'API_KEY'
 };
 
 export default Config;
