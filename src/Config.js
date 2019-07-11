7a8dae725331d9dfeb86b68586254848938374af
Miguel <miguel.lezcano@haintech.cl>
2019-07-11 12:27:04 -0400

    updated port

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 const Config = {
-    baseUrl: (process.env.NODE_ENV === 'production') ? 'https://floating-woodland-62237.herokuapp.com' : `${window.location.protocol}//${window.location.hostname}:8080`,
+    baseUrl: (process.env.NODE_ENV === 'production') ? 'https://floating-woodland-62237.herokuapp.com' : `${window.location.protocol}//${window.location.hostname}:3000`,
     mapKey: 'API_KEY'
 };
 
